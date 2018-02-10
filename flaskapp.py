9ead1115ff82497d3c49883281ca5287b6510df6
clementruin <clement.ruin@yahoo.fr>
2018-02-10 15:40:16 +0100

    bar legend D3

--- flaskapp.py
+++ flaskapp.py
@@ -5,7 +5,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
 @app.route('/')
 def home():
-    return render_template("index.html", city_index=1)
+    return render_template("index.html", city_index=1, city_code=78000)
 
 if __name__ == '__main__':
 	app.run(debug=True)
